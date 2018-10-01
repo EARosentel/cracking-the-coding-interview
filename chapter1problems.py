918a51879977f49b01689bc05bb3c7c4bc399213
Elisabeth Rosentel <ellierosentel@gmail.com>
2018-10-01 16:10:48 -0700

    Added 1.5 and 1.6

--- chapter1problems.py
+++ chapter1problems.py
@@ -1,4 +1,4 @@
-
+import BitVector
 # Problem 1.1
 # Is Unique: Implement and algorithm to determine if a string has all unique characters.
 # What if you couldn't use additional data structures
@@ -51,27 +51,152 @@ def urlify(urlstring, strlen):
         print(index)
     return finalstring
 
-#
-# stopping here for today
-#
 
+def urlifyinplace(urlstring, trulen):
+    spacecount = 0
 
-def urlifyinplace(urlstring, strlen):
-    index = 0
-    while index < len(urlstring):
-        if urlstring[index] == " ":
-            urlstring[index] = '%'
-            for letter in range(index, len(urlstring)):
-                urlstring[letter+2] = urlstring[letter]
-            index += 1
-            urlstring[index] = '2'
-            index += 1
-            urlstring[index] = '0'
+    for i in range(trulen):
+        if urlstring[i] == ' ':
+            spacecount += 1
 
-        index += 1
+    index = trulen + spacecount*2
+
+    for letter in range(trulen-1, -1, -1):
+        if urlstring[letter] == ' ':
+            urlstring[index-1] = '0'
+            urlstring[index-2] = '2'
+            urlstring[index-3] = '%'
+            index -= 3
+        else:
+            urlstring[index-1]=urlstring[letter]
+            index -= 1
 
     return urlstring
 
 
+# Problem 1.4
+# Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome.
 
+def permpalindrome(inputstr):
+    lettermap = {}
+
+    for letter in inputstr:
+        if letter == ' ':  #this one only takes into account space. it should account for any non letter character
+            continue
+
+        if letter in lettermap.keys():
+            lettermap.pop(letter)
+        else:
+            lettermap[letter] = 0
+
+    remainingletters = lettermap.keys()
+
+    if len(remainingletters) > 1:
+        return False
+
+    return True
+
+
+# Palindrome Permutation - with bit vectors
+
+
+def lettertoint(letter):
+    lettermap = {
+        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
+        'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19,
+        't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
+    }
+
+    if letter in lettermap.keys():
+        return lettermap[letter]
+    else:
+        return 0
+
+
+def toggle(bitvector, index):
+    if index == 0:
+        return bitvector
+    mask = BitVector.BitVector(size=26, intVal=1)
+    mask = mask << index
+    if int(bitvector & mask) == 0:
+        bitvector |= mask
+    else:
+        bitvector &= ~mask
+
+    return bitvector
+
+
+def createbitvector(phrase):
+    bitvector = BitVector.BitVector(size=26)
+    for letter in phrase:
+        x = lettertoint(letter)
+        bitvector = toggle(bitvector, x)
+    return bitvector
+
+
+def checkbitvector(bitvector):
+    return int(bitvector & BitVector.BitVector(size=26, intVal=int(bitvector)-1)) == 0
+
+
+def ispalindromepermutation(phrase):
+    bitvector = createbitvector(phrase)
+    return int(bitvector) == 0 or checkbitvector(bitvector)
+
+
+# Problem 1.5
+# One Away - There are three types of edits that can be performed on strings: insert a character, remove a character,
+# or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+
+def oneaway(string1, string2):
+    if len(string1) == len(string2):
+        flag = False
+        for index in range(len(string1)):
+            if string1[index: index+1] != string2[index: index+1]:
+                if flag:
+                    return False
+                else:
+                    flag = True
+        return True
+
+    else:
+        if len(string1) > len(string2):
+            return extracharoneaway(string1, string2)
+        else:
+            return extracharoneaway(string2, string1)
+
+
+def extracharoneaway(lstring, sstring):
+    specindex = 0
+    for index in range(len(sstring)):
+        if lstring[index+specindex: index+1+specindex] != sstring[index: index+1]:
+            if specindex == 1:
+                return False
+            else:
+                specindex = 1
+                if lstring[index + specindex: index + 1 + specindex] != sstring[index: index + 1]:
+                    return False
+    return True
+
+
+# Problem 1.6
+# String Compression: Implement a method to perform basic string compression using the counts of repeated characters.
+# If the compressed string would not be smaller than the original string, return the original string.
+
+
+def compresstring(inputstring):
+    index = 0
+    counter = 1
+    resultstring = ""
+    while index < len(inputstring):
+        if inputstring[index:index+1] == inputstring[index+1: index+2]:
+            counter += 1
+        else:
+            resultstring += inputstring[index:index+1] + str(counter)
+            counter = 1
+        index += 1
+
+    if len(resultstring) > len(inputstring):
+        return inputstring
+    else:
+        return resultstring
 
--- chapter1tests.py
+++ chapter1tests.py
@@ -1,7 +1,7 @@
 import chapter1problems
 
 if __name__ == "__main__":
-    # tests for problem 1.1
+    ############ tests for problem 1.1 ####################################################
     assert chapter1problems.isunique("token") is True, "String token failed isUnique test."
     assert chapter1problems.isunique("babe") is False, "String babe failed isUnique test."
     assert chapter1problems.isunique("") is True, "Empty string failed isUnique test."
@@ -13,19 +13,77 @@ if __name__ == "__main__":
     assert chapter1problems.isuniqueinplace("...") is False, "String ... failed isUniqueInPlace test."
     print("Problem 1.1 tests complete.")
 
-    # tests for problem 1.2
+    ############## tests for problem 1.2 ########################################################
     assert chapter1problems.ispermutation("toga", "goat") is True, "Permutation test failed for inputs: toga and goat."
     assert chapter1problems.ispermutation("boat", "bat") is False, "Permutation test failed for inputs: boat and bat."
-    assert chapter1problems.ispermutation("a gasp", "as pag") is True, "Permutation test failed for inputs: a gasp and as pag."
+    assert chapter1problems.ispermutation("a gasp", "as pag") is True, "Permutation test failed for inputs: a gasp " \
+                                                                       "and as pag."
     assert chapter1problems.ispermutation("who.", ".how") is True, "Permutation test failed for inputs: who. and .how "
-    assert chapter1problems.ispermutation("Faith", "faith") is True, "Permutation test failed for inputs: Faith and faith."
+    assert chapter1problems.ispermutation("Faith", "faith") is True, "Permutation test failed for inputs: Faith and " \
+                                                                     "faith."
     assert chapter1problems.ispermutation("", "") is True, "Permutation test failed for inputs: empty strings."
     print("Problem 1.2 tests complete.")
 
-    # tests for problem 1.3
-    assert chapter1problems.urlifyinplace(["a"," ","b"," ","c"," "," "," "," "], 5) == "a%20b%20c", "Urlify on 'a b c    ' has failed."
+    ############# tests for problem 1.3 ###########################################################
+    assert chapter1problems.urlifyinplace(["a", " ", "b", " ", "c", " ", " ", " ", " "], 5) == ['a', '%', '2', '0', 'b',
+                                                                                                '%', '2', '0',
+                                                                                                'c'], \
+        "Urlify on 'a b c    ' has failed."
+    assert chapter1problems.urlifyinplace(
+        ["m", "r", " ", "j", "o", "h", "n", " ", "s", "m", "i", "t", "h", " ", " ", " ", " "], 13) == ['m', 'r', '%',
+                                                                                                       '2', '0',
+                                                                                                       'j', 'o', 'h',
+                                                                                                       'n', '%', '2',
+                                                                                                       '0', 's', 'm',
+                                                                                                       'i', 't', 'h'],\
+        "Urlify on 'mr john smith' has failed."
+    assert chapter1problems.urlifyinplace(['a', 'c', 'd', 'c'], 4) == ['a', 'c', 'd', 'c'], \
+        "Urlify on acdc has failed."
+    assert chapter1problems.urlifyinplace([' ', ' ', ' ', ' '], 1) == ['%', '2', '0', ' '], \
+        "Urlify on all spaces has failed."
 
     print("Problem 1.3 tests complete.")
+
+    ################# tests for problem 1.4 ###########################################################
+    assert chapter1problems.permpalindrome("tacocat") is True, "Permpalindrome failed for tacocat."
+    assert chapter1problems.permpalindrome("taco cat") is True, "Permpalindrome failed for taco cat."
+    assert chapter1problems.permpalindrome("act o cat") is True, "Permpalindrome failed for act o cat."
+
+    assert chapter1problems.permpalindrome("taco ca") is False, "Permpalindrome failed for taco ca."
+    assert chapter1problems.permpalindrome("ababc") is True, "Permpalindrome failed for ababc."
+    assert chapter1problems.permpalindrome(" ") is True, "Permpalindrome failed for blank space."
+    assert chapter1problems.permpalindrome("abcde") is False, "Permpalindrome failed for abcde."
+
+
+
+    ############### tests for 1.4 with bit manipulation ##################################
+    assert chapter1problems.ispalindromepermutation("tacocat") is True, "Ispalindromepermutation failed for tacocat."
+    assert chapter1problems.ispalindromepermutation("taco cat") is True, "Ispalindromepermutation failed for taco cat."
+    assert chapter1problems.ispalindromepermutation("act.o cat") is True, "Ispalindromepermutation failed for " \
+                                                                          "act.o cat."
+
+    assert chapter1problems.ispalindromepermutation("taco ca") is False, "Ispalindromepermutation failed for taco ca."
+    assert chapter1problems.ispalindromepermutation("ababc") is True, "Ispalindromepermutation failed for ababc."
+    assert chapter1problems.ispalindromepermutation(" ") is True, "Ispalindromepermutation failed for blank space."
+    assert chapter1problems.ispalindromepermutation("abcde") is False, "Ispalindromepermutation failed for abcde."
+
+    print("Problem 1.4 tests complete.")
+
+    ############## tests for problem 1.5 ####################################################
+    assert chapter1problems.oneaway("bob", "bob") is True, "One away has failed for 'bob','bob'."
+    assert chapter1problems.oneaway("boat", "bot") is True, "One away has failed for 'boat','bot'."
+    assert chapter1problems.oneaway("boar", "bot") is False, "One away has failed for 'boar','bot'."
+    assert chapter1problems.oneaway("bow", "bot") is True, "One away has failed for 'bow','bot'."
+    assert chapter1problems.oneaway("", "") is True, "One away has failed for two empty strings."
+
+    print("Problem 1.5 tests complete.")
+
+    ############# tests for problem 1.6 ######################################################
+    assert chapter1problems.compresstring("aaabbbccdddd") == "a3b3c2d4", "Compress String failed for 'a3b3c2d4'."
+    assert chapter1problems.compresstring("aabbccd") == "aabbccd", "Compress String failed for 'aabbccd'."
+    assert chapter1problems.compresstring("abbbcbbbdbbb") == "a1b3c1b3d1b3", "Compress String failed for " \
+                                                                             "'abbbcbbbdbbb'."
+    print("Problem 1.6 tests complete.")
 
 
 
